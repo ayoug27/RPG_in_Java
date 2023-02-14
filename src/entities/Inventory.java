65329ef5215a8948143c8beea87385351fb2c787
ayoug <115540343+ayoug27@users.noreply.github.com>
2023-02-14 10:38:12 +0100

    Update Inventory.java

--- src/entities/Inventory.java
+++ src/entities/Inventory.java
@@ -3,15 +3,21 @@ import items.*;
 
 public class Inventory {
 	private items.Armor spareArmor;
-	public items.Weapon[] weaponsPocket;
-	public items.Artefact[] artefactsPocket; 
-	public items.Potion[] potionsPocket;
-	
+	private items.Weapon[] weaponsPocket;
+ private int itemsInWeaponsPocket;
+	private items.Artefact[] artefactsPocket;
+ private int itemsInArtefactsPocket;
+	private items.Potion[] potionsPocket;
+	private int itemsInPotionsPocket;
+
 	public Inventory() {
 		spareArmor = null;
 		this.weaponsPocket = new items.Weapon[2];
+		itemsInWeaponsPocket = 0;
 		this.artefactsPocket = new items.Artefact[3];
+		itemsInArtefactsPocket = 0;
 		this.potionsPocket = new items.Potion[5];
+  itemsInPotionsPocket = 0;
 	}
 
 	public items.Armor getSpareArmor() {
