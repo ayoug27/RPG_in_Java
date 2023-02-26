ab48c7507d64fa607483caf64266c0b06eaad98b
ayoug <ayoub.essalhia@gmail.com>
2023-02-26 20:13:45 +0100

    added functions for hero class

--- src/entities/Hero.java
+++ src/entities/Hero.java
@@ -15,12 +15,12 @@ public class Hero extends Entity {
 		super(x, y, 10, 1, 1, 1);
 		Level = 1;
 		XP = 0;
-		Attack = 1;
-		Defense = 1;
-		Speed = 1;
 		this.equippedWeapon = null;
 		this.equippedArmor = null;
 		this.equippedArtefact = null;
+		Attack = 1;
+		Defense = 1;
+		Speed = 1;
 		Inventory = new Inventory();
 	}
 	
@@ -39,20 +39,33 @@ public class Hero extends Entity {
 	public int getAttack() {
 		return Attack;
 	}
-	public void setAttack(int attack) {
-		Attack = attack;
+	public void setAttack() {
+		int itemAttack = 0;
+		if (this.equippedWeapon != null)
+			itemAttack += this.equippedWeapon.getAttack();
+		if (this.equippedArtefact != null && this.equippedArtefact.getBuffedAbility() == "Attack")
+			itemAttack += this.equippedArtefact.getBuffValue();
+		Attack = this.getBaseAttack()+itemAttack;	
 	}
 	public int getDefense() {
 		return Defense;
 	}
-	public void setDefense(int defense) {
-		Defense = defense;
+	public void setDefense() {
+		int itemDefense = 0;
+		if (this.equippedArmor != null)
+			itemDefense += this.equippedArmor.getDefense();
+		if (this.equippedArtefact != null && this.equippedArtefact.getBuffedAbility() == "Defense")
+			itemDefense += this.equippedArtefact.getBuffValue();
+		Defense = this.getBaseDefense()+itemDefense;	
 	}
 	public int getSpeed() {
 		return Speed;
 	}
-	public void setSpeed(int speed) {
-		Speed = speed;
+	public void setSpeed() {
+		int itemSpeed = 0;
+		if (this.equippedArtefact != null && this.equippedArtefact.getBuffedAbility() == "Speed")
+			itemSpeed += this.equippedArtefact.getBuffValue();
+		Speed = this.getBaseSpeed()+itemSpeed;	
 	}
 	public items.Weapon getEquippedWeapon() {
 		return equippedWeapon;
@@ -78,6 +91,60 @@ public class Hero extends Entity {
 	public void setInventory(entities.Inventory inventory) {
 		Inventory = inventory;
 	}
-	
-	
+	public void equipWeaponFromInventory(int i)
+	{
+		if (i <= this.getInventory().getItemsInWeaponsPocket()){
+			if (this.equippedWeapon == null) {
+				this.equippedWeapon = this.getInventory().accessWeaponInPocket(i);
+				this.getInventory().dropWeapon(i);
+			}
+			else {
+				items.Weapon actualEquippedWeapon = this.equippedWeapon;
+				this.equippedWeapon = this.getInventory().accessWeaponInPocket(i);
+				this.getInventory().dropWeapon(i);
+				this.getInventory().addWeaponInPocket(actualEquippedWeapon);
+			}
+		}
+		else {
+			System.err.println("Vous n'avez pas d'objet a cet endroit !");
+			return;
+		}
+	}	
+	public void equipArtefactFromInventory(int i)
+	{
+		if (i <= this.getInventory().getItemsInArtefactsPocket()){
+			if (this.equippedArtefact == null) {
+				this.equippedArtefact = this.getInventory().accessArtefactInPocket(i);
+				this.getInventory().dropArtefact(i);
+			}
+			else {
+				items.Artefact actualEquippedArtefact = this.equippedArtefact;
+				this.equippedArtefact = this.getInventory().accessArtefactInPocket(i);
+				this.getInventory().dropArtefact(i);
+				this.getInventory().addArtefactInPocket(actualEquippedArtefact);
+			}
+		}
+		else {
+			System.err.println("Vous n'avez pas d'objet a cet endroit !");
+			return;
+		}
+	}
+	public void equipArmorFromInventory()
+	{
+		if (this.getInventory().getSpareArmor() != null){
+			if (this.equippedArmor == null) {
+				this.equippedArmor = this.getInventory().getSpareArmor();
+				this.getInventory().setSpareArmor(null);
+			}
+			else {
+				items.Armor actualEquippedArmor = this.equippedArmor;
+				this.equippedArmor = this.getInventory().getSpareArmor();
+				this.getInventory().setSpareArmor(actualEquippedArmor);
+			}
+		}
+		else {
+			System.err.println("Vous n'avez pas d'objet a cet endroit !");
+			return;
+		}
+	}
 }
--- src/main.java
+++ src/main.java
@@ -8,22 +8,19 @@ public class main {
 	
 	public static void main(String[] args) throws Exception{
 		// TODO Auto-generated method stub
-/*
-		entities.Hero Hero = new Hero(0,0);
-		Hero.getInventory().addWeaponInPocket(new Weapon(0, "Epée en Bois","Common",1));
-		Hero.getInventory().addPotionInPocket(new Potion(0, "Potion de Vie","Common",5));
-		Hero.getInventory().addArtefactInPocket(new Artefact(0, "Talisman d'Attaque","Common","Attaque",5));
-		System.out.println(Hero.getInventory().accessPotionInPocket(1).toString());
-		System.out.println(Hero.getInventory().accessWeaponInPocket(1).toString());
-		System.out.println(Hero.getInventory().accessArtefactInPocket(1).toString());
-		Hero.getInventory().dropArtefact(1);
-		System.out.println(Hero.getInventory().getItemsInArtefactsPocket());
-*/
 		items.Weapon[][] weaponDatabase = Database.setWeaponDatabase("res/weaponDatabase.properties");
 		items.Potion[][] potionDatabase = Database.setPotionDatabase("res/potionDatabase.properties");
 		items.Armor[][] armorDatabase = Database.setArmorDatabase("res/armorDatabase.properties");
-		System.out.println(weaponDatabase[1][0].getName());
-		System.out.println(potionDatabase[5][0].getName());
-		System.out.println(armorDatabase[0][0].getName());
+		entities.Hero Hero = new Hero(0,0);
+		Hero.getInventory().addWeaponInPocket(weaponDatabase[0][9]);
+		Hero.getInventory().addPotionInPocket(potionDatabase[0][0]);
+		Hero.getInventory().addArtefactInPocket(new Artefact("Talisman d'Attaque","Common","Attaque",5));
+		System.out.println(Hero.getInventory().accessWeaponInPocket(1));
+		System.out.println(Hero.getAttack());
+		Hero.equipWeaponFromInventory(1);
+		Hero.setEquippedArtefact(new Artefact("Talisman d'Attaque","Common","Attack",5));
+		Hero.setAttack();
+		System.out.println(Hero.getAttack());
+		System.out.println(Hero.getInventory().getItemsInWeaponsPocket());
 	}
 }
