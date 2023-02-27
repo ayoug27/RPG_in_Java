756cc08d15d90dfce78aecda4863b9ced5d51e6a
ayoug <115540343+ayoug27@users.noreply.github.com>
2023-02-27 09:44:57 +0100

    Update Tile.java

--- src/Tile.java
+++ src/Tile.java
@@ -1,5 +1,13 @@
 
 
 public class Tile {
+    boolean isAccessible;
+    boolean hasEnnemy; 
+    boolean hasChest;
+    String Rarity;
+    entities.Ennemy ennemyContained;
+    Fight fight;
+    items.Item[] Chest;
 
+    
 }
